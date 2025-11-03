325034bb60237a886e7e199d54fcd495dda439a4
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2025-11-03 19:14:29 -0400

    Modificação na estrutura de arquivos da aplicação.

--- src/main/java/com/servidor/file_storage_Aparatar/repository/FileUriRepository.java
+++
@@ -1,9 +0,0 @@
-package com.servidor.file_storage_Aparatar.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.servidor.file_storage_Aparatar.models.File;
-
-@Repository
-public interface FileUriRepository extends JpaRepository <File, Long> {
-
-}

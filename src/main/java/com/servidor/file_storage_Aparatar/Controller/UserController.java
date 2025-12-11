3943c971ad78379b92596eb0f1ba5d8058085aba
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2025-12-10 23:14:26 -0400

    fix (UserController dependencies)

--- src/main/java/com/servidor/file_storage_Aparatar/Controller/UserController.java
+++ src/main/java/com/servidor/file_storage_Aparatar/Controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
